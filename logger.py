cce30b52aab2300d92fb0901ef07d5e6b22c047a
Ricky <rickywolf28@yahoo.com>
2019-10-03 13:21:20 -0700

    completed logger and most of simulation

--- logger.py
+++ logger.py
@@ -7,54 +7,42 @@ class Logger(object):
     # test them one by one as you write your class.
 
     def __init__(self, file_name):
-        # TODO:  Finish this initialization method. The file_name passed should be the
-        # full file name of the file that the logs will be written to.
         self.file_name = None
 
-    def write_metadata(self, pop_size, vacc_percentage, virus_name, mortality_rate,
-                       basic_repro_num):
-        '''
-        The simulation class should use this method immediately to log the specific
-        parameters of the simulation as the first line of the file.
-        '''
-        # TODO: Finish this method. This line of metadata should be tab-delimited
-        # it should create the text file that we will store all logs in.
-        # TIP: Use 'w' mode when you open the file. For all other methods, use
-        # the 'a' mode to append a new log to the end, since 'w' overwrites the file.
-        # NOTE: Make sure to end every line with a '/n' character to ensure that each
-        # event logged ends up on a separate line!
-        pass
+    def write_metadata(self, pop_size, vacc_percentage, virus_name, mortality_rate, basic_repro_num):
+        # This will open a 'w' file as data and write to it the pop_size, vacc_percentage, mortality_rate, basic_repro_num 
+        with self.open_file('w') as data:
+            data.write(f'{pop_size}\t{vacc_percentage}\t{virus_name}\t'
+                      f'{mortality_rate}\t{basic_repro_num}\n')
 
     def log_interaction(self, person, random_person, random_person_sick=None,
                         random_person_vacc=None, did_infect=None):
-        '''
-        The Simulation object should use this method to log every interaction
-        a sick person has during each time step.
+        # This will open a 'x' file as data and read through it to check if a person is infected of not infected 
+            with self.open_file('x') as data:
+                if did_infect:
+                    data.write(f'{person._id} infects {random_person._id}')
+                else:
+                    data.write(f'{person._id} didn\'t infect {random_person._id}')
 
-        The format of the log should be: "{person.ID} infects {random_person.ID} \n"
+                    if random_person_vacc:
+                        data.write(' because vaccinated')
+                    elif random_person_sick:
+                        data.write(' because already sick')
 
-        or the other edge cases:
-            "{person.ID} didn't infect {random_person.ID} because {'vaccinated' or 'already sick'} \n"
-        '''
-        # TODO: Finish this method. Think about how the booleans passed (or not passed)
-        # represent all the possible edge cases. Use the values passed along with each person,
-        # along with whether they are sick or vaccinated when they interact to determine
-        # exactly what happened in the interaction and create a String, and write to your logfile.
-        pass
+            data.write('\n')
 
     def log_infection_survival(self, person, did_die_from_infection):
-        ''' The Simulation object uses this method to log the results of every
-        call of a Person object's .resolve_infection() method.
 
-        The format of the log should be:
-            "{person.ID} died from infection\n" or "{person.ID} survived infection.\n"
-        '''
-        # TODO: Finish this method. If the person survives, did_die_from_infection
-        # should be False.  Otherwise, did_die_from_infection should be True.
-        # Append the results of the infection to the logfile
-        pass
+        # This will open a 'x' file as data and read through it to check if a person has died from an infection or not
+        with self.open_file('x') as data:
+            if did_die_from_infection:
+                data.write(f'{person._id} died from infection\n')
+            else:
+                data.write(f'{person._id} survived infection\n')
+            data.write('/n')
 
-    def log_time_step(self, time_step_number):
+    def log_time_step(self, time_step_number,newly_infected_count, newly_dead_count,
+                    total_infected, total_dead):
         ''' STRETCH CHALLENGE DETAILS:
 
         If you choose to extend this method, the format of the summary statistics logged
@@ -69,7 +57,10 @@ class Logger(object):
         The format of this log should be:
             "Time step {time_step_number} ended, beginning {time_step_number + 1}\n"
         '''
-        # TODO: Finish this method. This method should log when a time step ends, and a
-        # new one begins.
-        # NOTE: Here is an opportunity for a stretch challenge!
-        pass
+        with self.open_file('x') as data:
+            data.write(f'Time step {time_step_number} ended, '
+                      f'{newly_infected_count} are infected, '
+                      f'{newly_dead_count} recently died, '
+                      f'{total_infected} got infected, '
+                      f'{total_dead} have died, '
+                      f'beginning {time_step_number + 1}...\n')
--- simulation.py
+++ simulation.py
@@ -5,7 +5,7 @@ from logger import Logger
 from virus import Virus
 
 
-class Simulation(object):
+class Simulation():
     ''' Main class that will run the herd immunity simulation program.
     Expects initialization parameters passed as command line arguments when file is run.
 
@@ -13,42 +13,22 @@ class Simulation(object):
     population that are vaccinated, the size of the population, and the amount of initially
     infected people in a population are all variables that can be set when the program is run.
     '''
-    def __init__(self, pop_size, vacc_percentage, initial_infected=1, virus):
-        ''' Logger object logger records all events during the simulation.
-        Population represents all Persons in the population.
-        The next_person_id is the next available id for all created Persons,
-        and should have a unique _id value.
-        The vaccination percentage represents the total percentage of population
-        vaccinated at the start of the simulation.
-        You will need to keep track of the number of people currently infected with the disease.
-        The total infected people is the running total that have been infected since the
-        simulation began, including the currently infected people who died.
-        You will also need to keep track of the number of people that have die as a result
-        of the infection.
+    def __init__(self, population_size, vacc_percentage, virus_name,
+                 mortality_rate, basic_repro_num, initial_infected=1):
+        self.population_size = population_size
+        self.vacc_percentage = vacc_percentage
+        self.total_infected = initial_infected
+        self.current_infected = initial_infected
+        self.next_person_id = 0
+        self.total_dead = 0
 
-        All arguments will be passed as command-line arguments when the file is run.
-        HINT: Look in the if __name__ == "__main__" function at the bottom.
-        '''
-        # TODO: Create a Logger object and bind it to self.logger.
-        # Remember to call the appropriate logger method in the corresponding parts of the simulation.
-        # TODO: Call self._create_population() and pass in the correct parameters.
-        # Store the array that this method will return in the self.population attribute.
-        # TODO: Store each newly infected person's ID in newly_infected attribute.
-        # At the end of each time step, call self._infect_newly_infected()
-        # and then reset .newly_infected back to an empty list.
-        self.logger = None
-        self.population = [] # List of Person objects
-        self.pop_size = pop_size # Int
-        self.next_person_id = 0 # Int
-        self.virus = virus # Virus object
-        self.initial_infected = initial_infected # Int
-        self.total_infected = 0 # Int
-        self.current_infected = 0 # Int
-        self.vacc_percentage = vacc_percentage # float between 0 and 1
-        self.total_dead = 0 # Int
-        self.file_name = "{}_simulation_pop_{}_vp_{}_infected_{}.txt".format(
-            virus_name, pop_size, vacc_percentage, initial_infected)
+        self.virus = Virus(virus_name, mortality_rate, basic_repro_num)
+        file_name = (f'{virus_name}_simulation_pop_{population_size}_vp_'
+                     f'{vacc_percentage}_infected_{initial_infected}.txt')
+        self.logger = Logger(file_name)
+
         self.newly_infected = []
+        self.population = self._create_population(initial_infected)
 
     def _create_population(self, initial_infected):
         '''This method will create the initial population.
@@ -60,15 +40,21 @@ class Simulation(object):
                 list: A list of Person objects.
 
         '''
-        # TODO: Finish this method!  This method should be called when the simulation
-        # begins, to create the population that will be used. This method should return
-        # an array filled with Person objects that matches the specifications of the
-        # simulation (correct number of people in the population, correct percentage of
-        # people vaccinated, correct number of initially infected people).
+        population = []
+        infected_count = 0
+        while len(population) != self.population_size:
+            if infected_count != initial_infected:
+                population.append(Person(self.next_person_id,
+                                         is_vaccinated=False,
+                                         infection=self.virus))
+                infected_count += 1
+            else:
+                is_vaccinated = random.random() < self.vacc_percentage
+                population.append(Person(self.next_person_id, is_vaccinated))
 
-        # Use the attributes created in the init method to create a population that has
-        # the correct intial vaccination percentage and initial infected.
-        pass
+            self.next_person_id += 1
+
+        return population
 
     def _simulation_should_continue(self):
         ''' The simulation should only end if the entire population is dead
@@ -77,28 +63,23 @@ class Simulation(object):
             Returns:
                 bool: True for simulation should continue, False if it should end.
         '''
-        # TODO: Complete this helper method.  Returns a Boolean.
-        pass
+        return self.vacc_percentage==100 and self.total_dead == self.population_size
 
     def run(self):
         ''' This method should run the simulation until all requirements for ending
         the simulation are met.
         '''
-        # TODO: Finish this method.  To simplify the logic here, use the helper method
-        # _simulation_should_continue() to tell us whether or not we should continue
-        # the simulation and run at least 1 more time_step.
-
-        # TODO: Keep track of the number of time steps that have passed.
-        # HINT: You may want to call the logger's log_time_step() method at the end of each time step.
-        # TODO: Set this variable using a helper
         time_step_counter = 0
-        should_continue = None
 
-        while should_continue:
-        # TODO: for every iteration of this loop, call self.time_step() to compute another
-        # round of this simulation.
-            print('The simulation has ended after {} turns.'.format(time_step_counter))
-            pass
+        while self._simulation_should_continue():
+            (newly_infected_count, newly_dead_count) = self.time_step()
+            self.logger.log_time_step(time_step_counter,
+                                      newly_infected_count, newly_dead_count,
+                                      self.total_infected, self.total_dead)
+            time_step_counter += 1
+            
+        print(f'The simulation has ended after {time_step_counter} turns.')
+
 
     def time_step(self):
         ''' This method should contain all the logic for computing one time step
