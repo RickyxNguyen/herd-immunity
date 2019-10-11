ea34bf25738b5b1fba03ec4a6a39acc9f1f33b4f
soundreaper@gmail.com <soundreaper@gmail.com>
2019-10-10 17:14:54 -0700

    recreate first simulation unittest

--- test_simulation.py
+++ test_simulation.py
@@ -17,11 +17,17 @@ test_sim_1.run()
 test_sim_2.run()
 test_sim_3.run()
 
+with open("Ebola_simulation_pop_1000_vp_0.6_infected_1.txt", "rb") as a:
+    first_1 = a.readline()
+    a.seek(-2, os.SEEK_END)
+    while a.read(1) != b"\n":
+        a.seek(-2, os.SEEK_CUR)
+    last_1 = a.readline()
+
 class TestSimulation:
-    def simulation_test_1(self):
-        pass
-        #assert last == b"Time step 2 ended, 0 are infected, 20 recently died, 50 got infected, 42 have died, beginning 3...\n"
+    def test_1(self):
+        assert last_1 == b"Time step 3 ended, 0 are infected, 115 recently died, 420 got infected, 331 have died, beginning 4...\n"
     
-    def simulation_test_2(self):
+    def test_2(self):
         pass
         #assert last == b"Time step 2 ended, 0 are infected, 20 recently died, 50 got infected, 42 have died, beginning 3...\n"
