70a102654d4d533d62eb77a274a4240ed6179736
soundreaper@gmail.com <soundreaper@gmail.com>
2019-10-10 17:19:50 -0700

    add other 2 tests for 2 more simulations

--- test_simulation.py
+++ test_simulation.py
@@ -24,10 +24,26 @@ with open("Ebola_simulation_pop_1000_vp_0.6_infected_1.txt", "rb") as a:
         a.seek(-2, os.SEEK_CUR)
     last_1 = a.readline()
 
+with open("HIV_simulation_pop_1000_vp_0.1_infected_1.txt", "rb") as b:
+    first_2 = b.readline()
+    b.seek(-2, os.SEEK_END)
+    while b.read(1) != b"\n":
+        b.seek(-2, os.SEEK_CUR)
+    last_2 = b.readline()
+
+with open("Typhoid_simulation_pop_1000_vp_0.9_infected_1.txt", "rb") as c:
+    first_3 = c.readline()
+    c.seek(-2, os.SEEK_END)
+    while c.read(1) != b"\n":
+        c.seek(-2, os.SEEK_CUR)
+    last_3 = c.readline()
+
 class TestSimulation:
     def test_1(self):
         assert last_1 == b"Time step 3 ended, 0 are infected, 115 recently died, 420 got infected, 331 have died, beginning 4...\n"
     
     def test_2(self):
-        pass
-        #assert last == b"Time step 2 ended, 0 are infected, 20 recently died, 50 got infected, 42 have died, beginning 3...\n"
+        assert last_2 == b"Time step 3 ended, 0 are infected, 197 recently died, 900 got infected, 900 have died, beginning 4...\n"
+    
+    def test_3(self):
+        assert last_3 == b"Time step 6 ended, 0 are infected, 0 recently died, 112 got infected, 26 have died, beginning 7...\n"
