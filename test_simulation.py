f6627614bfee8635d22820dbbd7e18d3e97cc1c3
soundreaper@gmail.com <soundreaper@gmail.com>
2019-10-10 17:09:16 -0700

    create 3 viruses and run 3 simulations with them

--- test_simulation.py
+++ test_simulation.py
@@ -4,14 +4,38 @@ from virus import Virus
 from simulation import Simulation
 import os
 
+ebola = Virus("Ebola", 0.8, 0.5)
+hiv = Virus("HIV", 1.0, 0.4)
+typhoid = Virus("Typhoid", 0.2, 0.3)
+virus_list = [ebola, hiv, typhoid]
+
+test_sim_1 = Simulation(1000, 0.6, virus_list[0].name, virus_list[0].mortality_rate, virus_list[0].repro_rate)
+test_sim_2 = Simulation(1000, 0.1, virus_list[1].name, virus_list[1].mortality_rate, virus_list[1].repro_rate)
+test_sim_3 = Simulation(1000, 0.9, virus_list[2].name, virus_list[2].mortality_rate, virus_list[2].repro_rate)
+
+test_sim_1.run()
+test_sim_2.run()
+test_sim_3.run()
+
+with open("Ebola_simulation_pop_100_vp_0.5_infected_1.txt", "rb") as f:
+    first = f.readline()
+    f.seek(-2, os.SEEK_END)
+    while f.read(1) != b"\n":
+        f.seek(-2, os.SEEK_CUR)
+    last = f.readline()
+
+
 class TestSimulation:
-    def test_1(self):
-        test_virus = Virus("Ebola", 0.8, 0.5)
-        test_sim = Simulation(100, 0.5, test_virus.name, test_virus.mortality_rate, test_virus.repro_rate)
+    def simulation_test_1(self):
+        assert last == b"Time step 2 ended, 0 are infected, 20 recently died, 50 got infected, 42 have died, beginning 3...\n"
+    
+    def simulation_test_2(self):
+        test_virus = Virus("HIV", 0.3, 0.1)
+        test_sim = Simulation(1000, 0.2, test_virus.name, test_virus.mortality_rate, test_virus.repro_rate)
 
         test_sim.run()
 
-        with open("Ebola_simulation_pop_100_vp_0.5_infected_1.txt", "rb") as f:
+        with open("Ebola_simulation_pop_1000_vp_0.2_infected_1.txt", "rb") as f:
             first = f.readline()
             f.seek(-2, os.SEEK_END)
             while f.read(1) != b"\n":
