eb621a8d685f6bef14b27b348a9d8e92aa5e8cf8
soundreaper@gmail.com <soundreaper@gmail.com>
2019-10-10 19:34:32 -0700

    add pylint to pipfile and comment simulation tests

--- test_simulation.py
+++ test_simulation.py
@@ -4,11 +4,17 @@ from virus import Virus
 from simulation import Simulation
 import os
 
+"""
+The following creates 3 different types of Viruses and puts them in a list.
+"""
 ebola = Virus("Ebola", 0.8, 0.5)
 hiv = Virus("HIV", 1.0, 0.4)
 typhoid = Virus("Typhoid", 0.2, 0.3)
 virus_list = [ebola, hiv, typhoid]
 
+"""
+The following uses each of the Viruses previously created and runs the simulation.
+"""
 test_sim_1 = Simulation(1000, 0.6, virus_list[0].name, virus_list[0].mortality_rate, virus_list[0].repro_rate)
 test_sim_2 = Simulation(1000, 0.1, virus_list[1].name, virus_list[1].mortality_rate, virus_list[1].repro_rate)
 test_sim_3 = Simulation(1000, 0.9, virus_list[2].name, virus_list[2].mortality_rate, virus_list[2].repro_rate)
@@ -17,6 +23,9 @@ test_sim_1.run()
 test_sim_2.run()
 test_sim_3.run()
 
+"""
+The following opens each of the output files after the simulation is complete and outputs the last line in the file.
+"""
 with open("Ebola_simulation_pop_1000_vp_0.6_infected_1.txt", "rb") as a:
     first_1 = a.readline()
     a.seek(-2, os.SEEK_END)
@@ -38,6 +47,9 @@ with open("Typhoid_simulation_pop_1000_vp_0.9_infected_1.txt", "rb") as c:
         c.seek(-2, os.SEEK_CUR)
     last_3 = c.readline()
 
+"""
+The following compared the last lines of each file to the expected output as a unit test.
+"""
 class TestSimulation:
     def test_1(self):
         assert last_1 == b"Time step 3 ended, 0 are infected, 115 recently died, 420 got infected, 331 have died, beginning 4...\n"
